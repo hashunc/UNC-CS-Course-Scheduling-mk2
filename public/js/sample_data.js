e0b581f79e78b6e8de21ddf35c3d9bf5c3885e8e
Justin Indla <justinindla25@gmail.com>
2024-10-24 22:34:11 -0400

    New pages created + sample data

---
+++ public/js/sample_data.js
@@ -0,0 +1,213 @@
+export const data = [
+    {
+      "course_number": "CS101",
+      "section": "001",
+      "title": "Introduction to Computer Science",
+      "professor": "Dr. John Smith",
+      "time_slot": "MWF 9:00 AM - 10:00 AM"
+    },
+    {
+      "course_number": "CS102",
+      "section": "002",
+      "title": "Data Structures",
+      "professor": "Dr. Alice Johnson",
+      "time_slot": "T/Th 10:30 AM - 12:00 PM"
+    },
+    {
+      "course_number": "CS201",
+      "section": "003",
+      "title": "Algorithms",
+      "professor": "Dr. Karen Lee",
+      "time_slot": "MWF 11:00 AM - 12:00 PM"
+    },
+    {
+      "course_number": "CS202",
+      "section": "001",
+      "title": "Discrete Mathematics",
+      "professor": "Dr. Richard Evans",
+      "time_slot": "T/Th 9:00 AM - 10:30 AM"
+    },
+    {
+      "course_number": "CS301",
+      "section": "002",
+      "title": "Operating Systems",
+      "professor": "Dr. Susan White",
+      "time_slot": "MWF 10:00 AM - 11:00 AM"
+    },
+    {
+      "course_number": "CS302",
+      "section": "003",
+      "title": "Database Systems",
+      "professor": "Dr. Michael Brown",
+      "time_slot": "T/Th 1:30 PM - 3:00 PM"
+    },
+    {
+      "course_number": "CS303",
+      "section": "001",
+      "title": "Computer Networks",
+      "professor": "Dr. Carol Davis",
+      "time_slot": "MWF 2:00 PM - 3:00 PM"
+    },
+    {
+      "course_number": "CS304",
+      "section": "002",
+      "title": "Software Engineering",
+      "professor": "Dr. Brian Walker",
+      "time_slot": "T/Th 3:30 PM - 5:00 PM"
+    },
+    {
+      "course_number": "CS305",
+      "section": "001",
+      "title": "Artificial Intelligence",
+      "professor": "Dr. Angela Harris",
+      "time_slot": "MWF 12:00 PM - 1:00 PM"
+    },
+    {
+      "course_number": "CS306",
+      "section": "003",
+      "title": "Machine Learning",
+      "professor": "Dr. Mark Thompson",
+      "time_slot": "T/Th 10:30 AM - 12:00 PM"
+    },
+    {
+      "course_number": "CS401",
+      "section": "001",
+      "title": "Advanced Algorithms",
+      "professor": "Dr. Emma Clark",
+      "time_slot": "MWF 9:00 AM - 10:00 AM"
+    },
+    {
+      "course_number": "CS402",
+      "section": "002",
+      "title": "Compiler Design",
+      "professor": "Dr. George Martin",
+      "time_slot": "T/Th 1:30 PM - 3:00 PM"
+    },
+    {
+      "course_number": "CS403",
+      "section": "001",
+      "title": "Distributed Systems",
+      "professor": "Dr. Jennifer Scott",
+      "time_slot": "MWF 3:00 PM - 4:00 PM"
+    },
+    {
+      "course_number": "CS404",
+      "section": "002",
+      "title": "Computer Graphics",
+      "professor": "Dr. Paul Young",
+      "time_slot": "T/Th 9:00 AM - 10:30 AM"
+    },
+    {
+      "course_number": "CS405",
+      "section": "001",
+      "title": "Cryptography",
+      "professor": "Dr. Rebecca Hall",
+      "time_slot": "MWF 1:00 PM - 2:00 PM"
+    },
+    {
+      "course_number": "CS406",
+      "section": "003",
+      "title": "Computer Vision",
+      "professor": "Dr. Kevin Wright",
+      "time_slot": "T/Th 10:30 AM - 12:00 PM"
+    },
+    {
+      "course_number": "CS407",
+      "section": "001",
+      "title": "Quantum Computing",
+      "professor": "Dr. Julia Turner",
+      "time_slot": "MWF 10:00 AM - 11:00 AM"
+    },
+    {
+      "course_number": "CS408",
+      "section": "002",
+      "title": "Parallel Programming",
+      "professor": "Dr. Greg King",
+      "time_slot": "T/Th 1:30 PM - 3:00 PM"
+    },
+    {
+      "course_number": "CS409",
+      "section": "001",
+      "title": "Blockchain Technology",
+      "professor": "Dr. Margaret Adams",
+      "time_slot": "MWF 3:00 PM - 4:00 PM"
+    },
+    {
+      "course_number": "CS410",
+      "section": "002",
+      "title": "Mobile Application Development",
+      "professor": "Dr. Daniel Brooks",
+      "time_slot": "T/Th 9:00 AM - 10:30 AM"
+    },
+    {
+      "course_number": "CS411",
+      "section": "001",
+      "title": "Ethical Hacking",
+      "professor": "Dr. Nancy Reed",
+      "time_slot": "MWF 2:00 PM - 3:00 PM"
+    },
+    {
+      "course_number": "CS412",
+      "section": "003",
+      "title": "Natural Language Processing",
+      "professor": "Dr. Patrick Bell",
+      "time_slot": "T/Th 10:30 AM - 12:00 PM"
+    },
+    {
+      "course_number": "CS413",
+      "section": "001",
+      "title": "Advanced Database Systems",
+      "professor": "Dr. Lisa Powell",
+      "time_slot": "MWF 11:00 AM - 12:00 PM"
+    },
+    {
+      "course_number": "CS414",
+      "section": "002",
+      "title": "Big Data Analytics",
+      "professor": "Dr. Harold Green",
+      "time_slot": "T/Th 3:30 PM - 5:00 PM"
+    },
+    {
+      "course_number": "CS415",
+      "section": "001",
+      "title": "Cybersecurity",
+      "professor": "Dr. Linda Stewart",
+      "time_slot": "MWF 9:00 AM - 10:00 AM"
+    },
+    {
+      "course_number": "CS416",
+      "section": "002",
+      "title": "Human-Computer Interaction",
+      "professor": "Dr. James Price",
+      "time_slot": "T/Th 1:30 PM - 3:00 PM"
+    },
+    {
+      "course_number": "CS417",
+      "section": "001",
+      "title": "Cloud Computing",
+      "professor": "Dr. Elizabeth Morris",
+      "time_slot": "MWF 12:00 PM - 1:00 PM"
+    },
+    {
+      "course_number": "CS418",
+      "section": "003",
+      "title": "Software Testing",
+      "professor": "Dr. Gary Collins",
+      "time_slot": "T/Th 10:30 AM - 12:00 PM"
+    },
+    {
+      "course_number": "CS419",
+      "section": "001",
+      "title": "Web Development",
+      "professor": "Dr. Matthew Cooper",
+      "time_slot": "MWF 3:00 PM - 4:00 PM"
+    },
+    {
+      "course_number": "CS420",
+      "section": "002",
+      "title": "Game Development",
+      "professor": "Dr. Michelle Foster",
+      "time_slot": "T/Th 9:00 AM - 10:30 AM"
+    }
+  ]
+  
