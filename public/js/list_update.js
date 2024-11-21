e8f66c8016334a31230a6b8385d325681c1542aa
Justin Indla <justinindla25@gmail.com>
2024-11-21 17:52:15 -0500

    Update List changes
    Kinda working front end for update via list view

--- public/js/list_update.js
+++ public/js/list_update.js
@@ -1,3 +1,16 @@
+let clicked = [];
+
+function deleteFromClicked(string_in){
+    let i = 0;
+    while(i < clicked.length){
+        if(clicked[i] === string_in){
+            clicked = clicked.splice(i, i);
+            break;
+        }
+        i += 1;
+    }
+}
+
 fetch('http://127.0.0.1:8000')
   .then(response => response.json()
   )
@@ -6,11 +19,6 @@ fetch('http://127.0.0.1:8000')
     console.log(data);
 
     const board = document.getElementById('update-course-board');
-    // cell0.innerHTML = element['Course'];
-    // cell1.innerHTML = element['Section'];
-    // cell2.innerHTML = element['Title'];
-    // cell3.innerHTML = element['Professor'];
-    // cell4.innerHTML = element['Start Time'];
     data.forEach(element => {
         let small_div = document.createElement('div');
         small_div.innerText = `Course: ${element['Course']}\n
@@ -19,6 +27,27 @@ fetch('http://127.0.0.1:8000')
                                 Professor: ${element['Professor']}\n
                                 Start Time: ${element['Start Time']}`;
         small_div.className = 'update-small-div';
+        let checker = document.createElement('input');
+        checker.type = 'checkbox';
+        checker.className = 'update-list-check';
+        checker.dataset.course = element['Course']
+        checker.dataset.section = element['Section']
+        checker.dataset.title = element['Title']
+        checker.dataset.prof = element['Professor']
+        checker.dataset.start = element['Start Time']
+
+        checker.addEventListener('click', () => {
+            if(checker.checked){
+                console.log(`${checker.dataset.course} ${checker.dataset.section} added!`)
+                clicked.push(`${checker.dataset.course} ${checker.dataset.section}`);
+            } else {
+                console.log(`${checker.dataset.course} ${checker.dataset.section} removed!`)
+                deleteFromClicked(`${checker.dataset.course} ${checker.dataset.section}`);
+            }
+            console.log(clicked);
+        });
+
         board.appendChild(small_div);
+        board.appendChild(checker);
     });
   });
