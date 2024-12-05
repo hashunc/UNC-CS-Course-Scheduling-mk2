0f499ca3d59b6f0f9d6a9d3390682deb532ce3db
JustinIndla <justinindla25@gmail.com>
2024-12-05 18:06:45 -0500

    New changes for list-edit-view

--- public/js/list_update.js
+++ public/js/list_update.js
@@ -1,19 +1,28 @@
 let clicked = [];
 
-function deleteFromClicked(string_in){
+function compareJSONs(json_1, json_2){
+    let keys = Object.keys(json_1);
+    for(let key of keys){
+        if(json_1[key] != json_2[key]){
+            return false;
+        }
+    }
+    return true;
+}
+
+function deleteFromClicked(json_in){
     let i = 0;
     while(i < clicked.length){
-        if(clicked[i] === string_in){
-            clicked = clicked.splice(i, i);
-            break;
+        if(compareJSONs(clicked[i], json_in)){
+            clicked.splice(i, 1);
+            return;
         }
         i += 1;
     }
 }
 
 fetch('http://127.0.0.1:8000')
-  .then(response => response.json()
-  )
+  .then(response => response.json())
   .then(data => {
     data = data['schedule'];
     console.log(data);
@@ -25,16 +34,22 @@ fetch('http://127.0.0.1:8000')
                                 Section: ${element['Section']}\n
                                 Title: ${element['Title']}\n
                                 Professor: ${element['Professor']}\n
-                                Start Time: ${element['Start Time']}`;
+                                Start Time: ${element['Start Time']}\n
+                                Meeting Pattern: ${element['Meeting Pattern']}\n
+                                Seat Capacity: ${element['Seat Capacity']}\n
+                                Room: ${element['Room']}\n`;
         small_div.className = 'update-small-div';
         let checker = document.createElement('input');
         checker.type = 'checkbox';
         checker.className = 'update-list-check';
-        checker.dataset.course = element['Course']
-        checker.dataset.section = element['Section']
-        checker.dataset.title = element['Title']
-        checker.dataset.prof = element['Professor']
-        checker.dataset.start = element['Start Time']
+        checker.dataset.course = element['Course'];
+        checker.dataset.section = element['Section'];
+        checker.dataset.title = element['Title'];
+        checker.dataset.prof = element['Professor'];
+        checker.dataset.start = element['Start Time'];
+        checker.dataset.meeting = element['Meeting Pattern'];
+        checker.dataset.seat = element['Seat Capacity'];
+        checker.dataset.room = element['Room'];
 
         checker.addEventListener('click', () => {
             let course_data = {
@@ -42,7 +57,10 @@ fetch('http://127.0.0.1:8000')
                 'Section': checker.dataset.section,
                 'Title': checker.dataset.title,
                 'Prof': checker.dataset.prof,
-                'Start': checker.dataset.start
+                'Start': checker.dataset.start,
+                'Meeting Pattern': checker.dataset.meeting,
+                'Seat Capacity': checker.dataset.seat,
+                'Room': checker.dataset.room
             }
             if(checker.checked){
                 console.log(`${checker.dataset.course} ${checker.dataset.section} added!`)
@@ -51,6 +69,7 @@ fetch('http://127.0.0.1:8000')
                 console.log(`${checker.dataset.course} ${checker.dataset.section} removed!`)
                 deleteFromClicked(course_data);
             }
+            console.log(clicked);
         });
 
         board.appendChild(small_div);
@@ -63,7 +82,6 @@ fetch('http://127.0.0.1:8000')
     submit_button.id = 'update-list-submit';
 
     submit_button.addEventListener('click', () => {
-        //console.log(clicked);
         if(clicked.length === 0){
             alert('Please select at least 1 course to edit')
         } else {
