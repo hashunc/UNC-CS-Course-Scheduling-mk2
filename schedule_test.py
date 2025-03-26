548b288f7d1bb7ec0af5bbcddafeb9ceec039387
hashunc <jjacob3@unc.edu>
2025-03-26 19:05:58 +0000

    Implemented first sanity check test, aka base_case

--- schedule_test.py
+++ schedule_test.py
@@ -3,16 +3,30 @@ from schedule import *
 import pandas as pd
 from pathlib import Path
 
-home = Path.cwd()
+home = Path(".")
 testing_files: List[Path] = [
     home / "CSV" / "schedule_output.csv",
 ]
 matching_names: List[str] = [
-    "base_case.csv",
+    "base_case",
 ]
 matching_files: Dict[Path, Path] = {
-    home / "test_data" / name: home / "target_data" / name for name in matching_names
+    home / "test_data" / name: home / "target_data" / (name + ".csv")
+    for name in matching_names
 }
+
+
+# given outputted schedule and correct schedule, check if schedules are equivalent
+def compare_output():
+    for case in matching_files:
+        print([pd.read_csv(case / file) for file in ["data.csv", "room.csv"]])
+        service = CourseScheduler(case / "data.csv", case / "room.csv")
+        service.schedule_courses(case / "output.csv")
+        diff = pd.read_csv(case / "output.csv").compare(
+            pd.read_csv(matching_files[case])
+        )
+        print(diff)
+        assert diff.empty
 
 
 # read outputs into a data frame
@@ -23,18 +37,12 @@ def read_output() -> List[pd.DataFrame]:
         raise FileNotFoundError
 
 
-# given outputted schedule and constraints, check if all constraints are satisfied
-def test_constraints() -> None:
-
+# TODO: given outputted schedule and constraints, check if all constraints are satisfied
+def constraints() -> None:
     for schedule in read_output():
         for item in schedule:
             pass
 
 
-# given outputted schedule and correct schedule, check if schedules are equivalent
-def test_compare_output():
-    return
-
-
 def test_schedule_courses():
-    return
+    compare_output()
