1554cb5e63275040ba16a95b88aca56444230706
zmyunc <minyuan@unc.edu>
2025-04-27 17:20:44 -0400

    Add config.py

---
+++ scheduler/config.py
@@ -0,0 +1,16 @@
+# scheduler/config.py
+
+# Input files
+INPUT_COURSE_CAP = "data/Input/2025ClassEnrollCap.xlsx"
+INPUT_AVAILABILITY = "data/Input/Temple of Automatic course scheduling data sheet (Responses) - Form Responses 1.xlsx"
+INPUT_CLASSROOM = "data/Input/ClassRoom.xlsx"
+
+# Output files
+OUTPUT_ROOM_CSV = "data/CSV/room.csv"
+OUTPUT_NEW_DATA_CSV = "data/CSV/new_data.csv"
+OUTPUT_SCHEDULE_CSV = "data/CSV/schedule_output.csv"
+OUTPUT_SCHEDULE_OUTPUT_COPY = "data/Output/schedule_output.csv"
+OUTPUT_GOOGLE_CALENDAR_UNDERGRAD = "data/Output/google_calendar_undergraduate.csv"
+OUTPUT_GOOGLE_CALENDAR_GRAD = "data/Output/google_calendar_graduated.csv"
+OUTPUT_UNASSIGNED_COURSES = "data/Output/unassigned_courses.csv"
+OUTPUT_SKIPPED_FACULTY = "data/Output/skipped_faculty.csv"
